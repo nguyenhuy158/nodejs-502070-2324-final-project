7e873dbc5a821a2dcbabe824764e07d06295d75e
huy.ntq <hyquaq@gmail.com>
2023-10-12 08:42:44 +0700

    fix bug detail.pug

--- middlewares/auto-views.js
+++ middlewares/auto-views.js
@@ -4,10 +4,14 @@ autoViews = [];
 exports.autoViews = (req, res, next) => {
     const queryPath = req.path.toLowerCase().trim();
     const path = "./views/pages" + queryPath + "-auto.pug";
+    // console.log(fs.existsSync(path));
+    // console.log("=>(auto-views.js:9) __dirname + path", __dirname + path);
+    // console.log(fs.existsSync(__dirname + path));
     if (autoViews[queryPath]) return res.render(autoViews[queryPath]);
     
     if (fs.existsSync(path)) {
         autoViews[queryPath] = "pages/" + queryPath.replace(/^\//, "") + "-auto";
+        console.log("=>(auto-views.js:14) autoViews", autoViews);
         return res.render(autoViews[queryPath]);
     }
     next();
