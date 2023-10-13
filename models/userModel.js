7c6d8ac016ecd501a4341e5ca333ff57073323b7
huy.ntq <hyquaq@gmail.com>
2023-10-13 20:24:08 +0700

    test

--- models/userModel.js
+++ models/userModel.js
@@ -1,15 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const { formatTimestamp } = require('../middlewares/format');
-require('dotenv').config();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { formatTimestamp } = require("../middlewares/format");
+const passportLocalMongoose = require("passport-local-mongoose");
+require("dotenv").config();
 
 const userSchema = new Schema({
     email: { type: String, unique: true, required: true, trim: true },
     username: { type: String, trim: true, minlength: 1 },
     fullName: { type: String, trim: true, minlength: 2 },
-    role: { type: String, enum: [process.env.ROLE_ADMIN, process.env.ROLE_SALE], default: process.env.ROLE_SALE, lowercase: true },
+    role: {
+        type: String,
+        enum: [process.env.ROLE_ADMIN, process.env.ROLE_SALE],
+        default: process.env.ROLE_SALE,
+        lowercase: true
+    },
     password: { type: String, trim: true, minlength: 1, select: false },
     password_confirm: { type: String, trim: true, minlength: 1 },
     token: { type: String },
@@ -27,26 +33,26 @@ userSchema.methods.updateProfilePicture = function (newProfilePicture) {
     return this.save();
 };
 
-userSchema.pre('save', function (next) {
+userSchema.pre("save", function (next) {
     const user = this;
-
+    
     if (!user.username) {
-        const emailParts = user.email.split('@');
+        const emailParts = user.email.split("@");
         if (emailParts.length > 0) {
             user.username = emailParts[0];
             user.password = user.username;
             user.password_confirm = user.username;
         }
     }
-
-    if (!user.isModified('password')) return next();
-
+    
+    if (!user.isModified("password")) return next();
+    
     bcrypt.genSalt(10, (err, salt) => {
         if (err) return next(err);
-
+        
         bcrypt.hash(user.password, salt, (err, hash) => {
             if (err) return next(err);
-
+            
             user.password = hash;
             user.password_confirm = hash;
             next();
@@ -55,11 +61,11 @@ userSchema.pre('save', function (next) {
 });
 
 
-userSchema.virtual('createdAtFormatted').get(function () {
+userSchema.virtual("createdAtFormatted").get(function () {
     return formatTimestamp(this.createdAt);
 });
 
-userSchema.virtual('updatedAtFormatted').get(function () {
+userSchema.virtual("updatedAtFormatted").get(function () {
     return formatTimestamp(this.updatedAt);
 });
 
@@ -68,8 +74,10 @@ userSchema.methods.generateAccessJWT = function () {
         id: this._id,
     };
     return jwt.sign(payload, process.env.SECRET_ACCESS_TOKEN, {
-        expiresIn: '20m',
+        expiresIn: "20m",
     });
 };
 
-module.exports = mongoose.model('User', userSchema);
+userSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model("User", userSchema);
