0ae4ffd032093b974eaca1a56585b64c3bd841a9
huy.ntq <hyquaq@gmail.com>
2023-10-20 16:44:03 +0700

    feature lock/unlock

--- routes/user.js
+++ routes/user.js
@@ -5,15 +5,15 @@ const userController = require("../controllers/userController");
 router
     .get("/", userController.getUsers)
     .post("/", userController.createUser)
+    .put("/:id/lock", userController.lockAccount)
+    .put("/:id/unlock", userController.unlockAccount)
     .get("/resend/:id", userController.resendEmail)
-    .get("/delete/:id", userController.deleteUser)
     .get("/create-account", userController.getCreateAccount)
     .post("/create-account", userController.createAccount)
+    .delete("/:id", userController.deleteUser)
     .get("/:id", userController.getUser)
     .get("/login", userController.login)
-    .post("/login", userController.loginSubmit)
-    .get("/lock/:id", userController.lockAccount)
-    .get("/unlock/:id", userController.unlockAccount);
+    .post("/login", userController.loginSubmit);
 
 
 module.exports = router;
