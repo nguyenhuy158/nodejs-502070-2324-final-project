29a0e6b6774c22deb6f30654f955df362d6fd982
huy.ntq <hyquaq@gmail.com>
2023-11-02 11:26:42 +0700

    change validator to new file

--- routes/auth.js
+++ routes/auth.js
@@ -14,8 +14,29 @@ router
     .get("/login", authController.get)
     .get("/email-confirm", authController.emailConfirm)
     .get("/password-reset", authController.passwordResetGet)
-    .post("/password-reset",          authController.passwordReset)
-    .post("/login", passport.authenticate("local", passportAuthenticateConfig))
+    .post("/password-reset", authController.passwordReset)
+    .post("/login",
+          body("username")
+              .trim()
+              .notEmpty()
+              .withMessage("Username cannot be empty!"),
+          body("password")
+              .trim()
+              .notEmpty()
+              .withMessage("Password cannot be empty!")
+              .isLength({ min: 2 })
+              .withMessage("Password must have at least 6 characters!"),
+          (req, res, next) => {
+              const result = validationResult(req);
+              console.log(`=>(auth.js:33) result`, result);
+              if (result.errors.length === 0) {
+                  next();
+              } else {
+                  req.flash("success", result.errors[0].msg);
+                  res.redirect("/login");
+              }
+          },
+          passport.authenticate("local", passportAuthenticateConfig))
     .get("/logout", authController.logout);
 
 
