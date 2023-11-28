51562deaefaecf53820a087ad0592c71fd761e40
huy.ntq02 <huy.ntq02@gmail.com>
2023-11-28 23:26:23 +0700

    Update toastr config options

--- public/js/custom/toastr-config.js
+++ public/js/custom/toastr-config.js
@@ -1,16 +1,32 @@
 /* eslint-disable no-undef */
+// TODO uncomment this
+// $(() => {
+//     const notification = sessionStorage.getItem('notification')
+//     const notificationType = sessionStorage.getItem('notificationType')
+
+//     if (notification) {
+//         if (notificationType === 'success') {
+//             toastr.success(notification)
+//         } else if (notificationType === 'error') {
+//             toastr.error(notification)
+//         }
+
+//         sessionStorage.removeItem('notification')
+//         sessionStorage.removeItem('notificationType')
+//     }
+// })
 toastr.options = {
     closeButton: true,
-    debug: false,
-    newestOnTop: false,
+    debug: true,
+    newestOnTop: true,
     progressBar: true,
     positionClass: 'toast-top-right',
-    preventDuplicates: false,
+    preventDuplicates: true,
     onclick: null,
-    showDuration: '1000',
-    hideDuration: '10000',
+    showDuration: '3000',
+    hideDuration: '3000',
     timeOut: '5000',
-    extendedTimeOut: '1000',
+    extendedTimeOut: '5000',
     showEasing: 'swing',
     hideEasing: 'linear',
     showMethod: 'fadeIn',
